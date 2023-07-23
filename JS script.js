434fd4c30ec3b004f8e314eb236845fdb5210535
michael03nyamwaya <99642769+michael03nyamwaya@users.noreply.github.com>
2023-07-23 13:27:49 +0300

    Create JS script.js

---
+++ JS script.js
@@ -0,0 +1,16 @@
+//appending elements after they've been created
+	cont.appendChild(card)
+	card.appendChild(image)
+	card.appendChild(foodName)
+	card.appendChild(Price)
+	card.appendChild(Hotel)
+	card.appendChild(itemLink)
+	itemLink.appendChild(sbtn)
+	
+	}
+		getItems()
+	}
+	//Submit order
+
+	
+})
