bfffe3f78456f4dd2ba2ed6556e76bb9c2531305
michael03nyamwaya <99642769+michael03nyamwaya@users.noreply.github.com>
2023-07-23 13:31:17 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCkCAp-i2N9gUiTaTECfI4GvkMe6bWjrNQ",
+    authDomain: "nyamwayafoods.firebaseapp.com",
+    databaseURL: "https://nyamwayafoods-default-rtdb.firebaseio.com",
+    projectId: "nyamwayafoods",
+    storageBucket: "nyamwayafoods.appspot.com",
+    messagingSenderId: "450708611214",
+    appId: "1:450708611214:web:88b6c4d9c812f7d739e9df",
+    measurementId: "G-WSHVGDCD89"
+  };
+
+firebase.initializeApp(firebaseConfig)
